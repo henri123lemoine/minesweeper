029aa027b522d09ab653cf82a14563bc460a67a3
Henri Lemoine <henri123lemoine@gmail.com>
2024-12-28 05:06:38 -0500

    Updated lib.rs for solver

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,13 @@ pub mod board;
 pub mod error;
 pub mod game;
 pub mod position;
+pub mod solver;
 
-pub use board::{Board, Cell};
+pub use board::{Board, BoardIterator, Cell, RevealResult};
 pub use error::GameError;
 pub use game::{Action, Game, GameState};
 pub use position::Position;
+pub use solver::{
+    create_full_solver, Certainty, CountingSolver, MatrixSolver, ProbabilisticSolver, Solver,
+    SolverAction, SolverBoard, SolverResult,
+};
